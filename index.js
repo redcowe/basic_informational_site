37fb2d9f46dcb1918ec44253d952d24a3150a6f3
Joshua Cowell <joshuacowell@hotmail.com>
2022-06-13 23:11:38 +0000

    added index route

--- index.js
+++ index.js
@@ -1,53 +1,29 @@
 const http = require('http');
 const path = require('path');
 const { URL } = require('url');
+const fs = require('fs');
 require('dotenv').config();
 
 
 const PORT = process.env.PORT;
 
+
 const server = http.createServer((req, res) => {
-    res.statusCode = 200;
-    let parsedURL = URL.parse(/^\/+|\/+$/g, "");
-    console.log(path);
-    let qs = parsedURL.query;
-    let headers = req.headers;
-    let method = req.method.toLocaleUpperCase();
-
-    req.on("data", () => {
-        console.log("got some data");
-    })
-
-   
-    req.on("end", () => {
-        let route =
-            typeof routes[path] !== "undefined" ? routes[path] : routes["notFound"];
-        let data = {
-            path: path,
-            queryString: qs,
-            headers: headers,
-            method: method
+   if (req.url === '/index') {
+    fs.readFile('./index.html', (err, data) => {
+        if (err) {
+            res.writeHead(404, {'Content-Type': 'application/json'});
+            res.end({message: 'Page not found'});
         }
-        route(data, res)
-    })    
+        res.writeHead(200, {'Content-Type': 'text/html'});
+        res.end(data);
+    });   
+   }
 })
 
 server.listen(PORT, () => {
     console.log(`Server is running on 192.168.0.32:8080`);
 })
-
-const routes = {
-    index: (data, res) => {
-    
-    },
-    about: (data, res) => {
-
-    },
-    notFound: (data, res) => {
-        
-    }
-
-}
 
 
 
