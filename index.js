675c3dda23d7d6828e2c1bdf0be9f7797c2b7f04
Joshua Cowell <joshuacowell@hotmail.com>
2022-06-14 02:12:06 +0000

    finished add all routes

--- index.js
+++ index.js
@@ -1,23 +1,57 @@
 const http = require('http');
-const path = require('path');
-const { URL } = require('url');
 const fs = require('fs');
 require('dotenv').config();
 
 
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 3000;
 
+//Helper function for displaying 404 messages
+function displayNotFound() {
+    
+    fs.readFile('./404.html', (err, data) => {
+        if (err) {
+            console.log("Unable to read file");
+            process.exit(1);
+        }
+        return data;
+    })
+    
+}
 
 const server = http.createServer((req, res) => {
    if (req.url === '/index') {
     fs.readFile('./index.html', (err, data) => {
         if (err) {
             res.writeHead(404, {'Content-Type': 'application/json'});
-            res.end({message: 'Page not found'});
+            res.end(displayNotFound());
         }
         res.writeHead(200, {'Content-Type': 'text/html'});
         res.end(data);
     });   
+   }
+   else if (req.url === '/about') {
+    fs.readFile('./about.html', (err, data) => {
+        if (err) {
+            res.writeHead(404, {'Content-Type': 'application/json'});
+            res.end(displayNotFound());
+        }
+        res.writeHead(200, {'Content-Type': 'text/html'});
+        res.end(data);
+    })
+   }
+   else if (req.url === '/contact-me') {
+    fs.readFile('./contact-me.html', (err, data) => {
+        if (err) {
+            res.writeHead(404, {'Content-Type': 'application/json'});
+            res.end(displayNotFound());
+        }
+        res.writeHead(200, {'Content-Type': 'text/html'});
+        res.end(data);
+    })
+   }
+   else {
+    res.writeHead(404, {'Content-Type': 'text/html'});
+    res.end(displayNotFound());
    }
 })
 
