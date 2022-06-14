c9977bc357808b54be6c1b4fda5ea208939dffc6
Joshua Cowell <joshuacowell@hotmail.com>
2022-06-14 15:14:37 +0000

    finished adding routing

--- index.js
+++ index.js
@@ -5,25 +5,12 @@ require('dotenv').config();
 
 const PORT = process.env.PORT || 3000;
 
-//Helper function for displaying 404 messages
-function displayNotFound() {
-    
-    fs.readFile('./404.html', (err, data) => {
-        if (err) {
-            console.log("Unable to read file");
-            process.exit(1);
-        }
-        return data;
-    })
-    
-}
-
 const server = http.createServer((req, res) => {
-   if (req.url === '/index') {
+   if (req.url === '/index' || req.url === '/') {
     fs.readFile('./index.html', (err, data) => {
         if (err) {
-            res.writeHead(404, {'Content-Type': 'application/json'});
-            res.end(displayNotFound());
+            console.log("Unable to open file");
+            process.exit(1);
         }
         res.writeHead(200, {'Content-Type': 'text/html'});
         res.end(data);
@@ -32,8 +19,8 @@ const server = http.createServer((req, res) => {
    else if (req.url === '/about') {
     fs.readFile('./about.html', (err, data) => {
         if (err) {
-            res.writeHead(404, {'Content-Type': 'application/json'});
-            res.end(displayNotFound());
+            console.log("Unable to open file");
+            process.exit(1);
         }
         res.writeHead(200, {'Content-Type': 'text/html'});
         res.end(data);
@@ -42,21 +29,28 @@ const server = http.createServer((req, res) => {
    else if (req.url === '/contact-me') {
     fs.readFile('./contact-me.html', (err, data) => {
         if (err) {
-            res.writeHead(404, {'Content-Type': 'application/json'});
-            res.end(displayNotFound());
+            console.log("Unable to open file");
+            process.exit(1);
         }
         res.writeHead(200, {'Content-Type': 'text/html'});
         res.end(data);
     })
    }
    else {
-    res.writeHead(404, {'Content-Type': 'text/html'});
-    res.end(displayNotFound());
+    fs.readFile('./404.html', (err, data) => {
+        if (err) {
+            console.log("Unable to open file");
+            process.exit(1);
+        }
+        res.writeHead(404, {'Content-Type': 'text/html'});
+        res.end(data);
+    })
+    
    }
 })
 
 server.listen(PORT, () => {
-    console.log(`Server is running on 192.168.0.32:8080`);
+    console.log(`Server is running on 192.168.0.32:${PORT}`);
 })
 
 
